afcca6a0cb8bab14c38d3dbe94b764494d9deacf
Temporary <Temporary@Paul-XPS>
2019-12-03 15:10:39 -0800

    Updated hostname validation to use library

--- ispconfig.php
+++ ispconfig.php
@@ -1,4 +1,5 @@
 <?php
+use Blesta\Core\Util\Validate\Server;
 /**
  * Ispconfig Module.
  *
@@ -13,7 +14,7 @@ class Ispconfig extends Module
     /**
      * @var string The version of this module
      */
-    private static $version = '1.3.0';
+    private static $version = '1.4.0';
     /**
      * @var string The authors of this module
      */
@@ -1419,14 +1420,8 @@ class Ispconfig extends Module
      */
     public function validateHostName($host_name)
     {
-        if (strlen($host_name) > 255) {
-            return false;
-        }
-
-        return $this->Input->matches(
-            $host_name,
-            "/^([a-z0-9]|[a-z0-9][a-z0-9\-]{0,61}[a-z0-9])(\.([a-z0-9]|[a-z0-9][a-z0-9\-]{0,61}[a-z0-9]))+$/i"
-        );
+        $validator = new Server();
+        return $validator->isDomain($host_name) || $validator->isIp($host_name);
     }
 
     /**
